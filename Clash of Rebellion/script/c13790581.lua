c6858787984ecc6e4f69f0995ca1eb851a227900
ygohack137 <eduardoraton@hotmail.com>
2015-04-17 20:24:49 -0700

    Added Clash of Rebellion Scripts (17-04-15)

---
+++ Clash of Rebellion/script/c13790581.lua
@@ -0,0 +1,180 @@
+--The Winged Dragon of Ra – Sphere Mode
+function c13790581.initial_effect(c)
+	--summon with 3 tribute
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(82012319,2))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(c13790581.sum2con)
+	e1:SetCost(c13790581.sum2cost)
+	e1:SetTarget(c13790581.sum2tg)
+	e1:SetOperation(c13790581.sum2op)
+	c:RegisterEffect(e1)
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_LIMIT_SUMMON_PROC)
+	e1:SetCondition(c13790581.ttcon)
+	e1:SetOperation(c13790581.ttop)
+	e1:SetValue(SUMMON_TYPE_ADVANCE)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_LIMIT_SET_PROC)
+	e2:SetCondition(c13790581.setcon)
+	c:RegisterEffect(e2)
+	
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_CANNOT_ATTACK)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetValue(1)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetValue(1)
+	c:RegisterEffect(e5)
+	--special summon
+	local e6=Effect.CreateEffect(c)
+	e6:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e6:SetType(EFFECT_TYPE_IGNITION)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCost(c13790581.spcost)
+	e6:SetTarget(c13790581.sptg)
+	e6:SetOperation(c13790581.spop)
+	c:RegisterEffect(e6)
+	--cannot special summon
+	local e7=Effect.CreateEffect(c)
+	e7:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetCode(EFFECT_SPSUMMON_CONDITION)
+	c:RegisterEffect(e7)
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e8:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e8:SetCode(EVENT_SUMMON_SUCCESS)
+	e8:SetOperation(c13790581.regop)
+	c:RegisterEffect(e8)
+end
+function c13790581.ttcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>-3 and Duel.GetTributeCount(c)>=3
+end
+function c13790581.ttop(e,tp,eg,ep,ev,re,r,rp,c)
+	if c:GetControler()==tp then
+		local g=Duel.SelectTribute(tp,c,3,3)
+		c:SetMaterial(g)
+		Duel.Release(g,REASON_SUMMON+REASON_MATERIAL)
+		end
+	
+	if c:GetControler()==1-tp then		
+	local g=Duel.GetMatchingGroup(Card.IsReleasable,tp,0,MZONE,nil)
+		Duel.Hint(HINT_SELECTMSG,e:GetHandlerPlayer(),HINTMSG_RELEASE)
+		local sg=g:Select(tp,3,3,nil)
+		c:SetMaterial(sg)
+		Duel.Release(sg,REASON_SUMMON+REASON_MATERIAL)
+	end
+end
+function c13790581.setcon(e,c)
+	if not c then return true end
+	return false
+end
+
+function c13790581.sum2cost(e,c,tp)
+	return Duel.GetActivityCount(tp,ACTIVITY_NORMALSUMMON)==0
+end
+function c13790581.sum2con(e,tp)
+	return Duel.GetLocationCount(tp,0,LOCATION_MZONE)>-3
+end
+function c13790581.sum2tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetChainLimit(aux.FALSE)
+end
+function c13790581.sum2op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.SendtoHand(c,1-tp,REASON_EFFECT)
+	Duel.ShuffleHand(tp)
+	Duel.Summon(1-tp,c,true,nil)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetReset(RESET_PHASE+RESET_END)
+	e1:SetTargetRange(1,1)
+	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_CANNOT_MSET)
+	Duel.RegisterEffect(e2,tp)
+end
+
+
+function c13790581.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function c13790581.filter(c,e,tp)
+	return c:GetCode()==10000010 and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+end
+function c13790581.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
+		and Duel.IsExistingMatchingCard(c13790581.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+end
+function c13790581.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c13790581.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	local c=e:GetHandler()
+	local tc=g:GetFirst()
+	if not tc then return end
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SET_ATTACK)
+	e1:SetValue(4000)
+	e1:SetReset(RESET_EVENT+0xfe0000)
+	tc:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_SET_DEFENCE)
+	e2:SetValue(4000)
+	e2:SetReset(RESET_EVENT+0xfe0000)
+	tc:RegisterEffect(e2)
+	Duel.SpecialSummon(tc,0,tp,tp,true,true,POS_FACEUP)
+end
+
+function c13790581.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	e:GetHandler():RegisterFlagEffect(13790581,RESET_EVENT+0x1ec0000+RESET_PHASE+PHASE_END,0,1)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_END)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCondition(c13790581.controlcon)
+	e1:SetOperation(c13790581.controlop2)
+	c:RegisterEffect(e1)
+end
+function c13790581.controlcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(13790581)==0
+end
+function c13790581.controlop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_SET_CONTROL)
+	e4:SetValue(c:GetOwner())
+	c:RegisterEffect(e4)
+end
+function c13790581.ctval(e,c)
+	return e:GetHandlerPlayer()
+end
+
