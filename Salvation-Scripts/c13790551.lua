a92f82d919d01462d14e22f016cad58e92ebaf71
ygohack137 <eduardoraton@hotmail.com>
2015-04-23 13:31:10 -0700

    Clean1

--- Salvation-Scripts/c13790551.lua
+++
@@ -1,9 +0,0 @@
---The Claw of Hermos
-function c13790551.initial_effect(c)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e2:SetCode(EFFECT_ADD_CODE)
-	e2:SetValue(10000051)
-	c:RegisterEffect(e2)
-end
