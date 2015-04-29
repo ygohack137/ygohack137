8390c437f51b1ba836aa5fc487896e8423d40486
ygohack137 <eduardoraton@hotmail.com>
2015-04-29 01:04:41 -0700

    New CROSS TCG exclusive
    New Burning Abyss

--- Duelist of Destiny/script/c13720027.lua
+++ Duelist of Destiny/script/c13720027.lua
@@ -29,8 +29,8 @@ function c13720027.filter(c,e,tp)
 end
 function c13720027.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and c13720027.filter(chkc,e,tp) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 or Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(c13720027.filter,tp,0,LOCATION_GRAVE,1,nil,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(c13720027.filter,tp,0,LOCATION_GRAVE,1,nil,e,tp) and 
+	Duel.GetLocationCount(tp,LOCATION_MZONE)>0 or Duel.GetLocationCount(tp,LOCATION_SZONE)>0 end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,c13720027.filter,tp,0,LOCATION_GRAVE,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
