9b071867ab1a6cf0b2477fde57d6b38018006876
ygohack137 <eduardoraton@hotmail.com>
2015-08-17 04:35:12 -0700

    Synchro Monster Fix

--- Breaker of Shadow/script/c13790611.lua
+++ Breaker of Shadow/script/c13790611.lua
@@ -12,7 +12,6 @@ function c13790611.initial_effect(c)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
-	e1:SetCost(c13790611.descost)
 	e1:SetTarget(c13790611.destg)
 	e1:SetOperation(c13790611.desop)
 	c:RegisterEffect(e1)
@@ -33,26 +32,25 @@ function c13790611.cfilter(c,tp)
 		and Duel.IsExistingTarget(c13790611.dfilter,tp,0,LOCATION_MZONE,1,nil,c:GetAttack())
 end
 function c13790611.dfilter(c,atk)
-	return c:IsFaceup() and c:IsAttackBelow(atk) and c:IsDestructable()
-end
-function c13790611.descost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c13790611.cfilter,tp,LOCATION_REMOVED,0,1,nil,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,c13790611.cfilter,tp,LOCATION_REMOVED,0,1,1,nil,tp)
-	local atk=g:GetFirst():GetAttack()
-	e:SetLabel(atk)
-	Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
-end
-function c13790611.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingMatchingCard(c13790611.dfilter,tp,0,LOCATION_MZONE,1,nil,e:GetLabel()) end
-	local g=Duel.GetMatchingGroup(c13790611.dfilter,tp,0,LOCATION_MZONE,nil,e:GetLabel())
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,g:GetCount(),0,0)
-end
-function c13790611.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(c13790611.dfilter,tp,0,LOCATION_MZONE,nil,e:GetLabel())
-	Duel.ChangePosition(g,POS_FACEUP_DEFENCE,0,POS_FACEUP_DEFENCE,0)
+	return c:IsFaceup() and c:IsAttackBelow(atk)
 end
 
+function c13790611.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and c13790611.cfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c13790611.cfilter,tp,LOCATION_REMOVED,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,c13790611.cfilter,tp,LOCATION_REMOVED,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c13790611.desop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoDeck(tc,nil,2,REASON_EFFECT)
+		local g=Duel.GetMatchingGroup(c13790611.dfilter,tp,0,LOCATION_MZONE,nil,tc:GetAttack())
+		Duel.ChangePosition(g,POS_FACEUP_DEFENCE)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
 
 function c13790611.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_MZONE) end
