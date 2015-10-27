b89419e5b195836058278967daca7bcfac83caca
ygohack137 <eduardoraton@hotmail.com>
2015-10-27 10:19:07 -0700

    Ariadne Fix

--- Breaker of Shadow/script/c13790664.lua
+++ Breaker of Shadow/script/c13790664.lua
@@ -16,13 +16,15 @@ function c13790664.initial_effect(c)
 	e2:SetTargetRange(1,0)
 	e2:SetValue(c13790664.costchange)
 	c:RegisterEffect(e2)
+	--spsummon
 	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e3:SetCode(EVENT_DESTROYED)
-	e3:SetProperty(EFFECT_FLAG_DELAY)
-	e3:SetCondition(c13790664.regcon)
-	e3:SetTarget(c13790664.regtg)
-	e3:SetOperation(c13790664.regop)
+	e3:SetCondition(c13790664.spcon2)
+	e3:SetTarget(c13790664.sptg2)
+	e3:SetOperation(c13790664.spop2)
 	c:RegisterEffect(e3)
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD)
@@ -44,16 +46,22 @@ function c13790664.checktg(e,c)
 	return c:IsType(TYPE_COUNTER)
 end
 
-function c13790664.regcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return rp~=tp and c:GetPreviousControler()==tp and c:IsPreviousLocation(LOCATION_MZONE)
-		and (c:IsReason(REASON_EFFECT) or (c:IsReason(REASON_BATTLE)))
+function c13790664.spcon2(e,tp,eg,ep,ev,re,r,rp)
+	return bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0 and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
 end
-function c13790664.regtg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c13790664.spop2(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c13790664.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function c13790664.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,LOCATION_DECK,0,3,nil,TYPE_COUNTER) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_DECK)
 end
-function c13790664.regop(e,tp,eg,ep,ev,re,r,rp)
+function c13790664.spop2(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_DECK,0,nil,TYPE_TRAP)
 	if g:GetCount()>=3 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
