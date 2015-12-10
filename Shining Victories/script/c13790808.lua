6069262a10fc700beb3bcab4b49fc99efc299c5b
ygohack137 <eduardoraton@hotmail.com>
2015-12-10 02:01:25 -0800

    Shining Victory Update

--- Shining Victories/script/c13790808.lua
+++ Shining Victories/script/c13790808.lua
@@ -7,55 +7,80 @@ function c13790808.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
-	--destroy
+	--Maintenance cost
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_RELEASE+CATEGORY_DESTROY)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
 	e2:SetRange(LOCATION_PZONE)
 	e2:SetCountLimit(1)
-	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
-	e2:SetTarget(c13790808.destg)
+	e2:SetCondition(c13790808.descon)
 	e2:SetOperation(c13790808.desop)
 	c:RegisterEffect(e2)
-	--spsummon limit
+	--Release Limit
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e3:SetRange(LOCATION_PZONE)
+	e3:SetCode(EFFECT_CANNOT_RELEASE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCondition(c13790808.rcon)
+	e3:SetTarget(c13790808.rlimit)
 	e3:SetTargetRange(1,1)
-	e3:SetTarget(c13790808.sumlimit)
 	c:RegisterEffect(e3)
-	--cannot release
+	--SP Limit
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD)
-	e4:SetRange(LOCATION_PZONE)
-	e4:SetCode(EFFECT_CANNOT_RELEASE)
-	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e4:SetTarget(c13790808.sumlimit)
-	e4:SetTargetRange(1,1)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_FLIP)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetOperation(c13790808.flagop)
 	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e5:SetCondition(c13790808.flagcon)
+	c:RegisterEffect(e5)
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e6:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e6:SetTargetRange(1,1)
+	e6:SetCondition(c13790808.spcon)
+	e6:SetTarget(c13790808.splimit)
+	c:RegisterEffect(e6)
 end
-function c13790808.destg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetTurnPlayer()==tp end
-	if not Duel.CheckReleaseGroup(tp,nil,1,nil) then
-		Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetHandler(),1,0,0)
-	end
+function c13790808.descon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
 end
 function c13790808.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
-	if Duel.CheckReleaseGroup(tp,Card.IsReleasableByEffect,1,c) and Duel.SelectYesNo(tp,aux.Stringid(13790808,2)) then
-		local g=Duel.SelectReleaseGroup(tp,Card.IsReleasableByEffect,1,1,c)
+	local con=Duel.CheckReleaseGroup(tp,nil,1,nil)
+	local op=false
+	if con then op=Duel.SelectYesNo(tp,aux.Stringid(78371393,2)) end
+	if op then
+		local g=Duel.SelectReleaseGroup(tp,Card.IsReleasableByEffect,1,1,nil)
 		Duel.Release(g,REASON_EFFECT)
-	else Duel.Destroy(c,REASON_EFFECT) end
+	else
+		Duel.Destroy(c,REASON_EFFECT)
+	end
 end
-function c13790808.sumlimit(e,c,sump,sumtype,sumpos,targetp,se)
-	return c:IsLocation(LOCATION_EXTRA) and not c:IsSetCard(0x1374)
-	and (bit.band(e:GetHandler():GetSummonType(),SUMMON_TYPE_SPECIAL)==SUMMON_TYPE_PENDULUM
-	or bit.band(e:GetHandler():GetSummonType(),SUMMON_TYPE_FLIP)==SUMMON_TYPE_FLIP)
+function c13790808.rfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_MONSTER) and c:IsSetCard(0x1374)
 end
-function c13790808.sumlimit(e,c,tp,sumtp)
+function c13790808.rcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(c13790808.rfilter,tp,LOCATION_ONFIELD,0,1,nil)
+end
+function c13790808.rlimit(e,c)
 	return not c:IsSetCard(0x1374)
 end
-
+function c13790808.flagcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_PENDULUM
+end
+function c13790808.flagop(e,tp,eg,ep,ev,re,r,rp)
+	e:GetHandler():RegisterFlagEffect(13790808,RESET_EVENT+0x1fe0000,0,1)
+end
+function c13790808.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetFlagEffect(13790808)~=0
+end
+function c13790808.splimit(e,c,sump,sumtype,sumpos,targetp)
+	return c:IsLocation(LOCATION_EXTRA) and not c:IsSetCard(0x1374)
+end
