2b0d8f227862c19097013f75a3f297ce250add3f
ygohack137 <eduardoraton@hotmail.com>
2016-02-19 21:02:46 -0800

    Ebon Magician Location Fix

--- The Dark Illusion/script/c13790906.lua
+++ The Dark Illusion/script/c13790906.lua
@@ -64,13 +64,13 @@ function c13790906.spfilter2(c,e,tp)
 end
 function c13790906.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c13790906.spfilter2,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+		and Duel.IsExistingMatchingCard(c13790906.spfilter2,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
 end
 function c13790906.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c13790906.spfilter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,c13790906.spfilter2,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
